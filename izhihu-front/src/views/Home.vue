e541238c1e36e0d249efdb914bb37eb1413d39ea
qiuguanlin <578482558@qq.com>
2019-06-26 16:24:56 +0800

    发布任务

--- izhihu-front/src/views/Home.vue
+++ izhihu-front/src/views/Home.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="home">
-  </div>
+  <el-container class="home">
+    <el-header>
+      header
+    </el-header>
+    <el-container>
+      <el-main>
+        Main
+        </el-main>
+      <el-aside width="200px" style="background-color:green;height:200px">
+        Aside
+      </el-aside>
+    </el-container>
+    <el-footer>
+      Footer
+    </el-footer>
+  </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      searchBarFixed: true
+    }
+  },
+  methods: {
+  }
 }
 </script>
+
+<style lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
