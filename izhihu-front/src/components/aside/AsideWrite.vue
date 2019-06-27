174cc24b1091dff5b3fc86279ae8255e180f4f63
qiuguanlin <578482558@qq.com>
2019-06-27 09:00:04 +0800

    1

---
+++ izhihu-front/src/components/aside/AsideWrite.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 首页右侧写回答写文章写想法 -->
+  <div>
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'asideWrite',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ izhihu-front/src/views/topic/Topic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="topic">
+    <el-container class="main">
+        <el-main>
+          Main
+          </el-main>
+        <el-aside width="300px" style="background-color:green;height:200px">
+          Aside
+        </el-aside>
+      </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.topic {
+  .main {
+  }
+}
+
+</style>
