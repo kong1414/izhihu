c755930c8468f8580298894864b00132ad937de4
Frank <LemonBoyQi@163.com>
2019-06-27 14:12:41 +0800

    1

--- izhihu-front/src/components/aside/AsideCategory.vue
+++ izhihu-front/src/components/aside/AsideCategory.vue
@@ -1,41 +1,41 @@
 <template>
-  <!-- 首页右侧第二栏 -->
-  <div class="aside-write">
-    <el-card class="write-card">
-      <el-row>
-        <el-col :span="8">
-          <el-button type="text" class="write-button">
-            <i class="el-icon-s-order"/>
-            <p>live</p>
-          </el-button>
-        </el-col>
-        <el-col :span="8">
-          <el-button type="text" class="write-button">
-            <i class="el-icon-edit"/>
-            <p>书店</p>
-          </el-button>
-        </el-col>
-        <el-col :span="8">
-          <el-button type="text" class="write-button">
-            <i class="el-icon-paperclip"/>
-            <p>圆桌</p>
-          </el-button>
-        </el-col>
-        <el-col :span="8">
-          <el-button type="text" class="write-button">
-            <i class="el-icon-s-order"/>
-            <p>专栏</p>
-          </el-button>
-        </el-col>
-        <el-col :span="8">
-          <el-button type="text" class="write-button">
-            <i class="el-icon-s-order"/>
-            <p>收费咨询</p>
-          </el-button>
-        </el-col>
-      </el-row>
-    </el-card>
-  </div>
+<!-- 首页右侧第二栏 -->
+<div class="aside-write">
+  <el-card class="write-card">
+    <el-row>
+      <el-col :span="8">
+        <el-button type="text" class="write-button">
+          <i class="el-icon-s-order"/>
+          <p>live</p>
+        </el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="text" class="write-button">
+          <i class="el-icon-edit"/>
+          <p>书店</p>
+        </el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="text" class="write-button">
+          <i class="el-icon-paperclip"/>
+          <p>圆桌</p>
+        </el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="text" class="write-button">
+          <i class="el-icon-s-order"/>
+          <p>专栏</p>
+        </el-button>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="text" class="write-button">
+          <i class="el-icon-s-order"/>
+          <p>收费咨询</p>
+        </el-button>
+      </el-col>
+    </el-row>
+  </el-card>
+</div>
 </template>
 
 <script>
@@ -47,37 +47,34 @@ export default {
     }
   },
   methods: {
-   
-
-    }
   }
-
+}
 </script>
 
 <style lang="scss">
 .aside-write {
-  .write-card {
+.write-card {
+  text-align: center;
+  .el-card__body {
+    padding: 15px;
+  }
+  .write-button {
     text-align: center;
-    .el-card__body {
-      padding: 15px;
-    }
-    .write-button {
-      text-align: center;
-      i {
-        margin: 6px;
-        font-size: 30px;
-      }
-    }
-    .el-divider {
-      margin: 5px 0;
-    }
-    .list {
-      margin-left: 10px;
-      text-align: left;
-      span {
-        color: #76839b;
-      }
+    i {
+      margin: 6px;
+      font-size: 30px;
     }
   }
+  .el-divider {
+    margin: 5px 0;
+  }
+  .list {
+    margin-left: 10px;
+    text-align: left;
+    span {
+      color: #76839b;
+    }
+  }
+}
 }
 </style>
