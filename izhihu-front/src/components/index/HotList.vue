8c0f9edd4e90f4511a382113dbd27ab0af1334bc
Frank <LemonBoyQi@163.com>
2019-06-28 08:44:18 +0800

    热榜

--- izhihu-front/src/components/index/HotList.vue
+++ izhihu-front/src/components/index/HotList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hot-list">
-    <div>
-      <el-radio-group v-model="labels" size="medium">
+    <div class="button">
+      <el-radio-group v-model="labels" size="medium" text-color='#0084ff' fill='#B0C4DE'>
         <el-radio-button label="全站"></el-radio-button>
         <el-radio-button label="科学"></el-radio-button>
         <el-radio-button label="数码"></el-radio-button>
@@ -9,6 +9,13 @@
         <el-radio-button label="时尚"></el-radio-button>
         <el-radio-button label="影视"></el-radio-button>
       </el-radio-group>
+    </div>
+    <div class="hot-list-content">
+      <div v-for="i in [1]" :key="i" class="item">
+        asd
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -30,7 +37,11 @@ export default {
 <style lang="scss">
 .hot-list {
   height: 1000px;
-  background-color: black;
+  // background-color: black;
+  .button {
+    padding: 5px;
+    border-bottom:2px solid #E4E7ED
+  }
 }
 </style>
 
