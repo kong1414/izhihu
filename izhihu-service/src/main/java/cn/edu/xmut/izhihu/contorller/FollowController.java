f52adb6f8cedb45939d2155c801d3aacc073082e
qiuguanlin <578482558@qq.com>
2019-06-30 13:25:17 +0800

    后端：关注与点赞接口

--- izhihu-service/src/main/java/cn/edu/xmut/izhihu/contorller/FollowController.java
+++ izhihu-service/src/main/java/cn/edu/xmut/izhihu/contorller/FollowController.java
@@ -1,7 +1,9 @@
 package cn.edu.xmut.izhihu.contorller;
 
 import cn.edu.xmut.izhihu.pojo.common.ResultVO;
+import cn.edu.xmut.izhihu.service.FollowService;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,28 +18,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ali/follow")
 public class FollowController {
 
+    @Autowired
+    private FollowService followService;
+
     @ApiOperation("关注")
     @RequestMapping(value = "/inFollow", method = RequestMethod.POST)
-    public ResultVO infollow(String userId, String contentId) {
-        return null;
+    public ResultVO infollow(String userId, String contentId, int type) {
+        return followService.infollow(userId, contentId, type);
     }
 
     @ApiOperation("取消关注")
     @RequestMapping(value = "/unFollow", method = RequestMethod.POST)
     public ResultVO unfollow(String userId, String contentId) {
-        return null;
+        return followService.unfollow(userId, contentId);
     }
 
     @ApiOperation("点赞")
     @RequestMapping(value = "/like", method = RequestMethod.POST)
     public ResultVO like(String userId, String contentId) {
-
-        return null;
+        return followService.like(userId, contentId);
     }
 
-    @ApiOperation("取消点赞")
+    @ApiOperation("不赞同")
     @RequestMapping(value = "/unLike", method = RequestMethod.POST)
     public ResultVO unLike(String userId, String contentId) {
-        return null;
+        return followService.unlike(userId, contentId);
+    }
+
+    @ApiOperation("取消态度（取消点赞和不赞同）")
+    @RequestMapping(value = "/like", method = RequestMethod.POST)
+    public ResultVO cancelLike(String userId, String contentId) {
+        return followService.cancelLike(userId, contentId);
     }
 }
