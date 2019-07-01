0b0da3379f2f5d89e3d2e735dfcc6e19d81e8bf5
qiuguanlin <578482558@qq.com>
2019-07-01 08:53:54 +0800

    后台init

---
+++ izhihu-behind/src/views/Index.vue
@@ -0,0 +1,24 @@
+<!-- index -->
+<template>
+  <div class="index">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <h1>index</h1> -->
+    <img alt="Vue logo" src="../assets/test.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.index {
+
+}
+</style>
