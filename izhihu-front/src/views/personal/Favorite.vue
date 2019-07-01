5b890aec1edea452d84f134796c2124e54c89bdd
seddie97 <49058056+seddie97@users.noreply.github.com>
2019-07-01 23:09:27 +0800

    收藏页和收藏页弹窗

--- izhihu-front/src/views/personal/Favorite.vue
+++ izhihu-front/src/views/personal/Favorite.vue
@@ -4,15 +4,22 @@
       <el-card class="collection-card">
         <el-tabs v-model="activeName" @tab-click="handleClick">
           <el-tab-pane label="我关注的收藏夹" name="att-collection">
-            <div id="collectionList" v-for="collection in collections" :key="collection.id">
-              <div class="collectionName">{{collection.cname}}</div>
+            <div id="collectionList" v-for="collection in subCollections" :key="collection.id">
+              <div class="collectionName">{{collection.sname}}</div>
               <div class="collectiionDetail">
                 由 {{collection.author}} 创建 · {{collection.content}} 条内容 · {{collection.subs}} 人关注 · 取消关注
               </div>
+              <el-divider></el-divider>
             </div>
           </el-tab-pane>
           <el-tab-pane label="我创建的收藏夹" name="cteate-collection">
-            我创建的收藏夹
+            <div id="collectionList" v-for="collection in creaCollections" :key="collection.id">
+              <div class="collectionName">{{collection.cname}}</div>
+              <div class="collectiionDetail">
+              {{collection.content}} 条内容 · {{collection.subs}} 人关注
+              </div>
+              <el-divider></el-divider>
+            </div>
           </el-tab-pane>
         </el-tabs>
       </el-card>
@@ -34,9 +41,14 @@ export default {
   data () {
     return {
       activeName: 'att-collection',
-      collections:[
-        {cname:"搞机",author:"港真",content:"1",subs:"2"},
-        {cname:"穿搭",author:"快乐",content:"3",subs:"4"}]
+      subCollections:[
+        {sname:"搞机",author:"港真",content:"1",subs:"2"},
+        {sname:"穿搭",author:"快乐",content:"3",subs:"4"}
+      ],
+      creaCollections:[
+        {cname:'学习',content:'5',subs:'10'},
+        {cname:'运动',content:'10',subs:'5'}
+      ]
     }
   },
   methods: {
@@ -62,6 +74,8 @@ export default {
       font-weight: bold;
     }
     .collectiionDetail{
+      margin-top: 10px;
+      margin-bottom: -10px;
       color: #999;
     }
   }
