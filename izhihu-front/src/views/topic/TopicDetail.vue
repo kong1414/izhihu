14341e40021ad86a702d541851126100eed0fffa
seddie97 <49058056+seddie97@users.noreply.github.com>
2019-07-02 09:56:08 +0800

    话题详情页部分

--- izhihu-front/src/views/topic/TopicDetail.vue
+++ izhihu-front/src/views/topic/TopicDetail.vue
@@ -1,8 +1,25 @@
 <template>
 <!-- 收藏夹页，对于某个收藏夹而言的页面 -->
   <el-container class="topicDetail-container">
-    <el-main>
-
+    <el-main class="topDet">
+      <el-card>
+        <el-image
+          style="width: 100px; height: 100px;"
+          :src="url"
+          ></el-image>
+        
+            <span>{{topName}}</span>
+          
+      </el-card>
+      <el-card class="secCard">
+        <div slot="header" class="clearfix">
+          <span class="dis">讨论</span>
+          <el-button style="float: right; padding: 3px 0" type="text">更多内容</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+      </el-card>
     </el-main>
     <el-aside width="300px" style="background-color:#ecf5ff;min-height:200px">
       <aside-collection></aside-collection>
@@ -21,6 +38,9 @@ export default {
   },
   data () {
     return {
+      url:'',
+      topName:'高考',
+      topDet:''
     }
   },
   methods: {
@@ -32,6 +52,31 @@ export default {
 </script>
 <style lang="scss">
 .topicDetail-container {
+  .topDet{
+    .secCard{
+      margin-top: 20px;
+    }
+    .dis{
+      font-weight: bold;
+      font-size: 20px;
+    }
+  }
 
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both;
+  }
 }
 </style>
