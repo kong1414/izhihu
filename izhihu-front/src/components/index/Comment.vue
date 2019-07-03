601c596affd0e91d2fd4e97486230087b289ddd5
qiuguanlin <578482558@qq.com>
2019-07-03 22:52:52 +0800

    评论

---
+++ izhihu-front/src/components/index/Comment.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- 评论组件 -->
+  <div>
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comment',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
