ec77eecb2694346ef16203c0730bf91730b4d2e3
Your Name <email@example.com>
2019-07-04 11:52:14 +0800

    收藏详情页右侧栏更新

--- izhihu-front/src/views/personal/FavoriteDetail.vue
+++ izhihu-front/src/views/personal/FavoriteDetail.vue
@@ -2,21 +2,23 @@
 <!-- 收藏夹页，对于某个收藏夹而言的页面 -->
   <el-container class="favorite-container">
     <el-main>
+      <el-card class="favorite">
 
+      </el-card>
     </el-main>
     <el-aside width="300px" style="background-color:#ecf5ff;min-height:200px">
-      <el-card class="creator" :body-style="{ padding: '10px 0px 10px 20px' }">
+      <el-card class="creator" :body-style="{ padding: '20px 0px 10px 20px' }">
         <div>
-          <span style="font-weight:bold; font-size:15px; ">关于创建者</span>
+          <span style=" font-size:15px; ">关于创建者</span>
         </div>
         <div class="creator-message">
           <el-avatar class="img" shape="square" :fit="cover" :src="photoUrl"></el-avatar>
           <el-button type="text" class="btn-creator-name">昵称</el-button>
         </div>
       </el-card>
-      <el-card class="favorite-status" :body-style="{ padding: '10px 0px 5px 20px'}">
+      <el-card class="favorite-status" :body-style="{ padding: '18px 0px 5px 20px'}">
         <div>
-          <span style="font-weight:bold; font-size:15px; ">收藏夹状态</span>
+          <span style="font-size:15px; ">收藏夹状态</span>
         </div>
         <div class="time">
           <span style="font-size:12px; ">最近活动于 09:14</span>
