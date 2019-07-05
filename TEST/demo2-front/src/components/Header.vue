b7e3682cecab4a3c225ef9cee569822d5342ea34
qiuguanlin <578482558@qq.com>
2019-07-05 14:07:44 +0800

    init

---
+++ TEST/demo2-front/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    12312
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- TEST/demo2-front/src/views/Home.vue
+++ TEST/demo2-front/src/views/Home.vue
@@ -1,23 +1,60 @@
 <template>
   <el-container class="home-container">
-    <el-aside width="200px">
-      123
-    </el-aside>
-    <el-main>
-      
-      <router-view />
-    </el-main>
+    <el-header height="60px"
+               class="header">
+      <header-item></header-item>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu class="el-menu-vertical"
+                 :collapse="collapsed"
+                 default-active="/home/index"
+                 @open="handleOpen"
+                 @close="handleClose"
+                 background-color="#545c64"
+                 text-color="#fff"
+                 active-text-color="#ffd04b"
+                 router>
+          <el-menu-item index="/home/index">
+            <i class="el-icon-document"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-menu-item index="/home/index">
+            <i class="el-icon-document"></i>
+            <span slot="title">2</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">
+        <router-view />
+      </el-main>
+    </el-container>
+
   </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderItem from '../components/Header'
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HeaderItem
   }
 }
 </script>
+
+<style lang="scss">
+.home-container {
+  .header {
+    background-color: aqua;
+  }
+  .main {
+    background-color: bisque;
+  }
+}
+</style>
+
