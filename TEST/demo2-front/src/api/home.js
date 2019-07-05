99aafeec93590ca47c7badf7ecca761f7e56a37d
Frank <LemonBoyQi@163.com>
2019-07-05 15:09:29 +0800

    接口上传

--- TEST/demo2-front/src/api/home.js
+++ TEST/demo2-front/src/api/home.js
@@ -13,3 +13,21 @@ export const reqGetHostList = params => { // 获得 列出热榜
 export const reqUserInfo = params => { // 获得 列出热榜
   return axios.post(`${BASE_URL}/user/people?` + params).then(res => res.data)
 }
+
+// 图书管理页
+
+export const reqBookInfo = params => { // 获得 书籍列表
+  return axios.post(`${BASE_URL}/user/book/list?` + params).then(res => res.data)
+}
+
+export const reqBookcreate = params => { // 获得 书籍列表
+  return axios.post(`${BASE_URL}/user/book/create?` + params).then(res => res.data)
+}
+
+export const reqBookupdate = params => { // 获得 书籍列表
+  return axios.post(`${BASE_URL}/user/book/update?` + params).then(res => res.data)
+}
+
+export const reqBookdel = params => { // 获得 书籍列表
+  return axios.post(`${BASE_URL}/user/book/del?` + params).then(res => res.data)
+}
