f5fcb161ad908d66b14b3c107c36c09a5fe868a0
qiuguanlin <578482558@qq.com>
2019-07-05 15:19:53 +0800

    1

--- TEST/demo2-front/src/api/home.js
+++ TEST/demo2-front/src/api/home.js
@@ -4,30 +4,36 @@ import {
 } from './config'
 // 首页
 
-export const reqGetHostList = params => { // 获得 列出热榜
-  return axios.post(`${BASE_URL}/hostList/list`, params).then(res => res.data)
-}
-
-// 个人页
-
-export const reqUserInfo = params => { // 获得 列出热榜
-  return axios.post(`${BASE_URL}/user/people?` + params).then(res => res.data)
-}
-
 // 图书管理页
 
-export const reqBookInfo = params => { // 获得 书籍列表
+export const reqBookInfo = params => {
   return axios.post(`${BASE_URL}/user/book/list?` + params).then(res => res.data)
 }
 
-export const reqBookcreate = params => { // 获得 书籍列表
-  return axios.post(`${BASE_URL}/user/book/create?` + params).then(res => res.data)
+export const reqBookcreate = params => {
+  return axios.post(`${BASE_URL}/user/book/create`, params).then(res => res.data)
 }
 
-export const reqBookupdate = params => { // 获得 书籍列表
-  return axios.post(`${BASE_URL}/user/book/update?` + params).then(res => res.data)
+export const reqBookupdate = params => {
+  return axios.post(`${BASE_URL}/user/book/update?`, params).then(res => res.data)
 }
 
-export const reqBookdel = params => { // 获得 书籍列表
+export const reqBookdel = params => {
   return axios.post(`${BASE_URL}/user/book/del?` + params).then(res => res.data)
+}
+
+export const reqBorrow = params => {
+  return axios.post(`${BASE_URL}/user/book/borrow`, params).then(res => res.data)
+}
+
+export const reqRebook = params => {
+  return axios.post(`${BASE_URL}/user/book/rebook`, params).then(res => res.data)
+}
+
+export const reqMyRecordList = params => {
+  return axios.post(`${BASE_URL}/user/book/myRecordList?` + params).then(res => res.data)
+}
+
+export const reqRecordAllList = params => {
+  return axios.post(`${BASE_URL}/user/book/recordAllList?` + params).then(res => res.data)
 }
